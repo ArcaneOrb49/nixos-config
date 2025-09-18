7fa54554d87c1a7093dda43b9126986212afb36c
Arvid <arvid.meltzer@gmail.com>
2025-09-18 15:54:56 +0200

    Added Neofetch

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,11 +115,12 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    discord
-    brave
-    git
-    kitty
+	wget
+	discord
+	brave
+	git
+	kitty
+	neofetch
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

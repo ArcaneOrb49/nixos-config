7a99b1e464f536784c69a22cb7737d9e024f3747
Arvid <arvid.meltzer@gmail.com>
2025-09-19 12:31:19 +0200

    system upgrade on 2025-09-19 12:31:19

--- flake.nix
+++ flake.nix
@@ -1,70 +1,58 @@
 {
-  description = "A very basic flake";
+  description = "NixOS config with Hyprland from flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs, hyprland }:
-    let
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, hyprland, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [ hyprland.overlays.default ];
+    };
+  in {
+    nixosConfigurations.myNixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./nixos/configuration.nix
 
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-	overlays = [ hyprland.overlays.default ];
-      };
-    in
-    {
-      nixosConfigurations = {
-        myNixos = nixpkgs.lib.nixosSystem {
-          inherit system;
-	  specialArgs = { inherit hyprland; }; # pass hyprland into modules
-          modules = [
-            ./nixos/configuration.nix
+        # Inline module for Hyprland
+        {
+          programs.hyprland = {
+            enable = true;
+            package = hyprland.packages.${system}.default;
+          };
 
+          services.greetd = {
+            enable = true;
+            settings = {
+              default_session = {
+                command = "Hyprland";
+                user = "arvid";
+              };
+            };
+          };
 
- # Inline module that applies Hyprland + overlay
-            {
-              programs.hyprland.enable = true;
-              programs.hyprland.package = hyprland.packages.${system}.default;
+          hardware.graphics.enable = true;
 
-              nixpkgs.overlays = [ hyprland.overlays.default ];
-	}
+          xdg.portal = {
+            enable = true;
+            #extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+          };
 
-{
-  services.greetd.enable = true;
-  services.greetd.settings = {
-    default_session = {
-      command = "Hyprland";
-      user = "arvid";
+          security.polkit.enable = true;
+
+          environment.systemPackages = with pkgs; [
+            vscode
+            # add whatever else you need here
+          ];
+        }
+      ];
     };
   };
-
-  # Graphics + portals (recommended)
-  hardware.graphics.enable = true;            # new name on recent NixOS
-  xdg.portal.enable = true;
-#  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  security.polkit.enable = true;              # for portals / permissions
-}
-
-
-
-   
-        {
-	environment.systemPackages = with pkgs; [
-    	vscode
-  	];
-	}
-          ];
-        };
-      };
-
-      packages.${system} = {
-        hello = nixpkgs.legacyPackages.${system}.hello;
-        default = self.packages.${system}.hello;
-      };
-    };
 }
 
